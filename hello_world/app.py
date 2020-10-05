cdb4f812e7f7cfb66b5a31b50dd984f1b6d5dc09
Pavle Neskovic <pavlenesko@gmail.com>
2020-10-05 12:39:02 +0200

    Add checkip to the response

--- hello_world/app.py
+++ hello_world/app.py
@@ -1,7 +1,5 @@
 import json
-
 import requests
-
 
 def lambda_handler(event, context):
     """Sample pure Lambda function
@@ -37,6 +35,6 @@ def lambda_handler(event, context):
         "statusCode": 200,
         "body": json.dumps({
             "message": "hello world",
-            # "location": ip.text.replace("\n", "")
+            "location": ip.text.replace("\n", "")
         }),
     }
